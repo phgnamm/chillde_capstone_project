9d019d3411d04ca7f44c38410cbfd78a5b61caa3
phgnam <kiss01646803595@gmail.com>
2024-12-24 16:36:11 +0700

    fix : update code Item

--- Chillde.API/Controllers/ItemController.cs
+++ Chillde.API/Controllers/ItemController.cs
@@ -1,5 +1,6 @@
 ﻿using Chillde.Services.Interfaces;
 using Chillde.Services.Models.CategoryModels;
+using Chillde.Services.Models.ItemModels;
 using Chillde.Services.Models.ResponseModels;
 using Chillde.Services.Services;
 using Microsoft.AspNetCore.Http;
@@ -25,6 +26,24 @@ namespace Chillde.API.Controllers
             try
             {
                 var result = await _itemService.GetById(id);
+                return StatusCode(result.Code, result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseModel
+                {
+                    Code = StatusCodes.Status500InternalServerError,
+                    Message = ex.Message
+                });
+            }
+        }
+        //      [Authorize]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, [FromForm] ItemUpdateModel itemUpdateModel)
+        {
+            try
+            {
+                var result = await _itemService.Update(id, itemUpdateModel);
                 return StatusCode(result.Code, result);
             }
             catch (Exception ex)
--- Chillde.Repositories/Interfaces/IItemRepository.cs
+++ Chillde.Repositories/Interfaces/IItemRepository.cs
@@ -1,7 +1,9 @@
 ﻿using Chillde.Repositories.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +11,7 @@ namespace Chillde.Repositories.Interfaces
 {
     public interface IItemRepository : IGenericRepository<Item>
     {
+        Task<Item?> GetFirstOrDefaultAsync(Expression<Func<Item, bool>> predicate);
+       
     }
 }
