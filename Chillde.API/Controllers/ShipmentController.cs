7fc10367ba09aa01d010c744e81d4b86eafe526a
phgnam <kiss01646803595@gmail.com>
2025-01-10 22:16:38 +0700

    update

---
+++ Chillde.API/Controllers/ShipmentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chillde.API.Controllers
+{
+    [Route("api/v1/shipments")]
+    [ApiController]
+    public class ShipmentController : ControllerBase
+    {
+    }
+}
