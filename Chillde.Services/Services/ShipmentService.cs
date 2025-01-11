f41cc047fa67e176457dab5bb7eda5db27270ba7
phgnam <kiss01646803595@gmail.com>
2025-01-12 00:28:04 +0700

    feat : calculate  fee  api ghn

--- Chillde.Services/Services/ShipmentService.cs
+++ Chillde.Services/Services/ShipmentService.cs
@@ -1,13 +1,88 @@
 ﻿using Chillde.Services.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Chillde.Repositories.Models.ShipmentModels;
+using Chillde.Services.Models.ResponseModels;
+using Chillde.Services.Models.ShipmentModels;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Chillde.Services.Services
 {
     public class ShipmentService : IShipmentService
     {
+        private readonly HttpClient _httpClient;
+
+
+        public ShipmentService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("GhnClient");
+        }
+
+
+        public async Task<ResponseModel> CalculateShippingFeeAsync(ShippingFeeRequestModel? requestModel)
+        {
+            if (requestModel == null || requestModel.Weight <= 0 || string.IsNullOrEmpty(requestModel.ToWardCode))
+            {
+                return new ResponseModel
+                {
+                    Code = StatusCodes.Status400BadRequest,
+                    Message = "Invalid shipping fee request",
+                    Data = null
+                };
+            }
+
+            var requestPayload = new
+            {
+                from_district_id = requestModel.FromDistrictId,
+                service_id = requestModel.ServiceId,
+                service_type_id = requestModel.ServiceTypeId,
+                to_district_id = requestModel.ToDistrictId,
+                to_ward_code = requestModel.ToWardCode,
+                height = requestModel.Height,
+                length = requestModel.Length,
+                weight = requestModel.Weight,
+                width = requestModel.Width,
+                insurance_value = requestModel.InsuranceValue
+            };
+
+            var content = new StringContent(JsonConvert.SerializeObject(requestPayload), Encoding.UTF8,
+                "application/json");
+
+            try
+            {
+                var response = await _httpClient.PostAsync("/v2/shipping-order/fee", content);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new ResponseModel
+                    {
+                        Code = (int)response.StatusCode,
+                        Message = "Failed to calculate shipping fee",
+                        Data = null
+                    };
+                }
+
+                var responseContent = await response.Content.ReadAsStringAsync();
+                var jsonObject = JsonConvert.DeserializeObject<JObject>(responseContent);
+                var feeData = jsonObject?["data"]?.ToObject<ShippingFeeResponseModel>();
+
+                return new ResponseModel
+                {
+                    Code = StatusCodes.Status200OK,
+                    Message = "Shipping fee calculated successfully",
+                    Data = feeData
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseModel
+                {
+                    Code = StatusCodes.Status500InternalServerError,
+                    Message = "An error occurred while calculating shipping fee",
+                    Data = ex.Message
+                };
+            }
+        }
     }
 }
