3650aeba50b7ab84382b270dd109d8f28048a28a
phgnam <kiss01646803595@gmail.com>
2025-02-27 11:24:12 +0700

    update code shipment status

--- Chillde.Services/Interfaces/IShipmentService.cs
+++ Chillde.Services/Interfaces/IShipmentService.cs
@@ -1,13 +1,16 @@
-﻿using Chillde.Services.Models.ResponseModels;
+﻿using Chillde.Repositories.Models.ShipmentModels;
+using Chillde.Services.Models.ResponseModels;
 using Chillde.Services.Models.ShipmentModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Chillde.Services.Interfaces
 {
     public interface IShipmentService
     {
         Task<ResponseModel> CalculateShippingFeeAsync(ShippingFeeRequestModel requestModel);
-        Task<ResponseModel> GetShipmentDetailAsync(string orderCode);
-        Task<ResponseModel> SwitchToReturnStatusAsync(string orderCode);
+        Task<CancelShipmentResponseModel> CancelShipmentAsync(string trackingOrder);
+        Task<byte[]> GetShippingLabelAsync(string trackingOrder);
+        Task<ResponseModel> GetOrderStatusAsync(string trackingOrder);
 
     }
 }
