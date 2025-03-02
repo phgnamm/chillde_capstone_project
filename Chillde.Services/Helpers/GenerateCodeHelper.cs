97bfb30c33c0cdcc3a0859f2b85ba2cd632defe0
tuannguyen30th <nguyenngoctuanthct@gmail.com>
2025-03-02 13:19:51 +0700

    fix: code logic

---
+++ Chillde.Services/Helpers/GenerateCodeHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chillde.Services.Helpers
+{
+    public class GenerateCodeHelper
+    {
+        private static readonly Random _random = new Random();
+        private const string Characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        public static string GenerateOrderCode()
+        {
+            const string prefix = "ORDCHD";
+            var randomPart = new string(Enumerable.Repeat(Characters, 8)
+                .Select(s => s[_random.Next(s.Length)]).ToArray());
+            return prefix + randomPart;
+        }
+    }
+}
