f7266659bcc4f08c537419f1346c9d7fab2cdf57
tinhcao <trancao134@gmail.com>
2025-03-12 21:05:50 +0700

    fix: helper namespace

--- Chillde.API/Controllers/BadWordFilterController.cs
+++ Chillde.API/Controllers/BadWordFilterController.cs
@@ -1,4 +1,4 @@
-﻿using Chillde.API.Helper;
+﻿using Chillde.API.Helpers;
 using Chillde.Services.Interfaces;
 using Chillde.Services.Models.BadWordFilterModels;
 using Chillde.Services.Models.ResponseModels;
