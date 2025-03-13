16ef434583a3b39cd00050d1090fd65c09f4e6a3
tinhcao <trancao134@gmail.com>
2025-03-13 11:39:03 +0700

    fix: add responseTime in offer and package

--- Chillde.Repositories/Entities/Offer.cs
+++ Chillde.Repositories/Entities/Offer.cs
@@ -11,6 +11,7 @@ public class Offer : BaseEntity
     public DateTime? DeliveryTime { get; set; }
     public int? SketchRevision { get; set; }
     public decimal? Price { get; set; }
+    public float? ResponseTime { get; set; }
 
     // Foreign key
     public Guid RequestId { get; set; }
