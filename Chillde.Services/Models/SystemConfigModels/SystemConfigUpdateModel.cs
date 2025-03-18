8d49f5d8bce1b82cd02dfd9b4a8d65f82131f199
tinhcao <trancao134@gmail.com>
2025-03-18 23:38:20 +0700

    fix: update model of config

---
+++ Chillde.Services/Models/SystemConfigModels/SystemConfigUpdateModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Chillde.Repositories.Models.SystemConfigModel;
+
+public class SystemConfigUpdateModel
+{
+    [Required]
+    public object Value { get; set; } = null!;
+}
