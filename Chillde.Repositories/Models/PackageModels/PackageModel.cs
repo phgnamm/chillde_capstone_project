877a99736201ce0b34ff92dfe9483e89471c3107
Tn Lc_ <lamtanloc3@gmail.com>
2025-03-20 20:42:44 +0700

    Refactor PackageModel to use PackageName enum
    Updated the `Name` property in `PackageModel` from a nullable string to a `PackageName` enum. Added the necessary namespace import for `Chillde.Repositories.Enums`. Adjusted instance creation in `ServiceService` to assign the new `PackageName` type to the `Name` property.

--- Chillde.Repositories/Models/PackageModels/PackageModel.cs
+++ Chillde.Repositories/Models/PackageModels/PackageModel.cs
@@ -1,11 +1,12 @@
 ﻿using Chillde.Repositories.Entities;
+using Chillde.Repositories.Enums;
 using Chillde.Repositories.Models.FeatureModels;
 
 namespace Chillde.Repositories.Models.PackageModels
 {
     public class PackageModel : BaseEntity
     {
-        public string? Name { get; set; }
+        public PackageName Name { get; set; }
         public string? Description { get; set; }
         public decimal? Price { get; set; }
         public Guid? ServiceId { get; set; }
