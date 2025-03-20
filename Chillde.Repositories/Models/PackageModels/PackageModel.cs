40a66234fcc3d77156f5b779da8969388a8d8649
Tn Lc_ <lamtanloc3@gmail.com>
2025-03-20 21:20:17 +0700

    update feature model and package model

--- Chillde.Repositories/Models/PackageModels/PackageModel.cs
+++ Chillde.Repositories/Models/PackageModels/PackageModel.cs
@@ -9,7 +9,12 @@ namespace Chillde.Repositories.Models.PackageModels
         public PackageName Name { get; set; }
         public string? Description { get; set; }
         public decimal? Price { get; set; }
+        public int? DeliveryTime { get; set; }
+        public int? SketchRevision { get; set; }
+        public float ResponseTime { get; set; }
+
         public Guid? ServiceId { get; set; }
+        public Guid? OfferId { get; set; }
         public List<FeatureModel> Features { get; set; }
     }
 }
