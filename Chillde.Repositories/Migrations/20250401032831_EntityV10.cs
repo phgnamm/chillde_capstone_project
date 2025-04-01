2a8771b710d696bfd36a4fa15af2f82f831562f2
phgnam <kiss01646803595@gmail.com>
2025-04-01 10:32:52 +0700

    udpate database shipment

---
+++ Chillde.Repositories/Migrations/20250401032831_EntityV10.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chillde.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class EntityV10 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StatusId",
+                table: "Shipment",
+                newName: "CurrentStatusId");
+
+            migrationBuilder.CreateTable(
+                name: "ShipmentStatusHistory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ShipmentId = table.Column<Guid>(type: "uuid", nullable: false),
+                    StatusId = table.Column<int>(type: "integer", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedById = table.Column<Guid>(type: "uuid", nullable: true),
+                    ModificationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    ModifiedById = table.Column<Guid>(type: "uuid", nullable: true),
+                    DeletionDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DeletedById = table.Column<Guid>(type: "uuid", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShipmentStatusHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShipmentStatusHistory_Shipment_ShipmentId",
+                        column: x => x.ShipmentId,
+                        principalTable: "Shipment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShipmentStatusHistory_ShipmentId",
+                table: "ShipmentStatusHistory",
+                column: "ShipmentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShipmentStatusHistory");
+
+            migrationBuilder.RenameColumn(
+                name: "CurrentStatusId",
+                table: "Shipment",
+                newName: "StatusId");
+        }
+    }
+}
