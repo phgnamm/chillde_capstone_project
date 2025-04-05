8ce0cbefa344563077fdb7793ff03b60e69d0b1b
Dang Van Hai <vanhaicntt5@gmail.com>
2025-04-05 09:44:14 +0700

    feat: get current roles

--- Chillde.Services/Models/ShippingAddressModels/ShippingAddressFilterModel.cs
+++ Chillde.Services/Models/ShippingAddressModels/ShippingAddressFilterModel.cs
@@ -9,6 +9,6 @@ namespace Chillde.Services.Models.ShippingAddressModels
 {
     public class ShippingAddressFilterModel : FilterParameter
     {
-        public bool IsDefault { get; set; }
+        public bool? IsDefault { get; set; }
     }
 }
