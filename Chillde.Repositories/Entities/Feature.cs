79f5c5c4323741c927a8f31602d0a0f36c4e96f0
Tn Lc_ <lamtanloc3@gmail.com>
2025-04-11 10:05:35 +0700

    Add MinQuantity and Index properties to entities
    - Introduced `MinQuantity` and `Index` properties in `Feature`, `Package`, and `PackageFeature` classes.
    - Updated `AppDbContextModelSnapshot` to reflect new properties in the database schema.
    - Created migration class `EntityV15` for adding new columns to relevant tables.
    - Modified `BuildTargetModel` to include new properties in the model builder.
    - Removed unnecessary regions for improved code readability.
    - Ensured new properties are initialized with default values.

--- Chillde.Repositories/Entities/Feature.cs
+++ Chillde.Repositories/Entities/Feature.cs
@@ -5,15 +5,13 @@ namespace Chillde.Repositories.Entities;
 public class Feature : BaseEntity
 {
     public string? Name { get; set; }
-
-    #region new fields
     public string? Question { get; set; } 
     public MediaType QuestionType { get; set; } = MediaType.Text;
     public bool IsInformationRequired { get; set; }
     public bool IsQuantity { get; set; }
-    #endregion
+    public int Index { get; set; }
 
-    
+
     // Relationship
     public virtual ICollection<PackageFeature> PackageFeatures { get; set; } = new List<PackageFeature>();
 }
