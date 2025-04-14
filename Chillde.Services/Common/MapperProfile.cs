1a743071f3b4e9dd25208ecba973604817db7cde
Tn Lc_ <lamtanloc3@gmail.com>
2025-04-14 16:57:47 +0700

    Add voucher management features and account updates
    - Implemented `GetAll` method in `VoucherController` for retrieving vouchers with filtering.
    - Added `ShippingAddresses` property to `AccountModel` for multiple shipping addresses.
    - Updated `MapperProfile` to include mappings for `VoucherModel` and `Voucher`.
    - Enhanced `IVoucherService` with a new `GetAll` method.
    - Introduced `Status` property in `AccountFilterModel` for account filtering.
    - Updated `OrderFilterModel` to support price filtering.
    - Modified `AccountService` to retrieve shipping addresses.
    - Enhanced `OrderService` to include price filtering based on user roles.
    - Added `GetAll` method in `VoucherService` with complex filtering logic.
    - Created `VoucherFilterModel` to define filtering criteria for vouchers.

--- Chillde.Services/Common/MapperProfile.cs
+++ Chillde.Services/Common/MapperProfile.cs
@@ -7,12 +7,14 @@ using Chillde.Repositories.Models.FAQModels;
 using Chillde.Repositories.Models.FeatureModels;
 using Chillde.Repositories.Models.ItemModels;
 using Chillde.Repositories.Models.MessageModels;
+using Chillde.Repositories.Models.OrderModels;
 using Chillde.Repositories.Models.PackageFeatureModels;
 using Chillde.Repositories.Models.PackageModels;
 using Chillde.Repositories.Models.ServiceModels;
 using Chillde.Repositories.Models.ShipmentModels;
 using Chillde.Repositories.Models.ShippingAddressModels;
 using Chillde.Repositories.Models.SubCategoryModels;
+using Chillde.Repositories.Models.VoucherModels;
 using Chillde.Services.Models.AccountModels;
 using Chillde.Services.Models.CategoryModels;
 using Chillde.Services.Models.FeatureModels;
@@ -103,8 +105,13 @@ public class MapperProfile : Profile
 
         //Order
         CreateMap<OrderAddModel, Order>().ReverseMap();
+        CreateMap<OrderModel, Order>().ReverseMap();
 
         //Shipment
         CreateMap<ShipmentModel, Shipment>().ReverseMap();
+
+        //Voucher
+        CreateMap<VoucherModel, Voucher>().ReverseMap();
+        CreateMap<Voucher, VoucherModel>().ReverseMap();
     }
 }
---
+++ Chillde.Services/Models/VoucherModels/VoucherFilterModel.cs
@@ -0,0 +1,12 @@
+﻿using Chillde.Repositories.Enums;
+using Chillde.Services.Common;
+
+namespace Chillde.Services.Models.VoucherModels
+{
+    public class VoucherFilterModel : FilterParameter
+    {
+        public VoucherStatus? Status { get; set; }
+        public decimal? MinDiscountValue { get; set; } = 0;
+        public decimal? MaxDiscountValue { get; set; } = 0;
+    }
+}
