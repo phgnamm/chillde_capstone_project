6aaaf58491eefa5f79f5cd4626abd45c4dae8c7e
tinhcao <trancao134@gmail.com>
2025-04-15 14:30:32 +0700

    fix: getOffer

--- Chillde.Services/Models/OfferModels/OfferFilterModel.cs
+++ Chillde.Services/Models/OfferModels/OfferFilterModel.cs
@@ -7,12 +7,12 @@ namespace Chillde.Services.Models.OfferModels
     {
         public Guid? RequestId { get; set; }
         public Guid? ServiceId { get; set; }
-        public Guid? CreatedById { get; set; }
         public OfferStatus? Status { get; set; }
         public int? MinDeliveryTime { get; set; }
         public int? MaxDeliveryTime { get; set; }
         public decimal? MinPrice { get; set; }
         public decimal? MaxPrice { get; set; }
         public Guid? CategoryId { get; set; }
+        public bool? IsDelete { get; set; }
     }
 }
