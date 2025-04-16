0785b70823128238072833de0847ad6194615cde
Tn Lc_ <lamtanloc3@gmail.com>
2025-04-16 08:22:54 +0700

    fix conflict

--- Chillde.API/Configuration.cs
+++ Chillde.API/Configuration.cs
@@ -386,6 +386,7 @@ public static class Configuration
 
         //OrderCompletionService
         services.AddHostedService<OrderCompletionService>();
+
         //Notification
         services.AddScoped<INotificationRepository, NotificationRepository>();
         services.AddScoped<INotificationService, NotificationService>();
