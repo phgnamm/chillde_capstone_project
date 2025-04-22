52af3c399db62768b5fa62471b8c3430370478a6
tuannguyen30th <nguyenngoctuanthct@gmail.com>
2025-04-22 20:59:34 +0700

    fix: voucher

--- Chillde.Services/Models/VoucherModels/VoucherFilterModel.cs
+++ Chillde.Services/Models/VoucherModels/VoucherFilterModel.cs
@@ -9,6 +9,7 @@ namespace Chillde.Services.Models.VoucherModels
         public VoucherStatus? Status { get; set; }
         public decimal? MinOrderValue { get; set; }
         public decimal? MaxDiscountValue { get; set; }
+        public decimal? MinDiscountValue { get; set; }
         public DateTime? StartTime { get; set; }
         public DateTime? ExpiredTime { get; set; }
         public int? MinReputation {  get; set; }
--- Chillde.Services/Services/VoucherService.cs
+++ Chillde.Services/Services/VoucherService.cs
@@ -242,6 +242,7 @@ namespace Chillde.Services.Services
                                          (!voucherFilterModel.IsDeleted.HasValue || voucher.IsDeleted == voucherFilterModel.IsDeleted) &&
                                          (!voucherFilterModel.MinOrderValue.HasValue || voucher.MinOrderValue >= voucherFilterModel.MinOrderValue) &&
                                          (!voucherFilterModel.MaxDiscountValue.HasValue || voucher.DiscountValue <= voucherFilterModel.MaxDiscountValue) &&
+                                         (!voucherFilterModel.MinDiscountValue.HasValue || voucher.DiscountValue >= voucherFilterModel.MinDiscountValue) &&
                                          (!voucherFilterModel.VoucherType.HasValue || voucher.VoucherType == voucherFilterModel.VoucherType) &&
                                          (!voucherFilterModel.StartTime.HasValue || voucher.StartTime >= voucherFilterModel.StartTime) &&
                                          (!voucherFilterModel.ExpiredTime.HasValue || voucher.ExpiredTime <= voucherFilterModel.ExpiredTime) &&
@@ -267,6 +268,10 @@ namespace Chillde.Services.Services
                                                   return voucherFilterModel.OrderByDescending
                                                       ? s.OrderByDescending(s => s.Receiver!.Username)
                                                       : s.OrderBy(s => s.Receiver!.Username);
+                                              case "reputation":
+                                                  return voucherFilterModel.OrderByDescending
+                                                      ? s.OrderByDescending(s => s.MinReputation)
+                                                      : s.OrderBy(s => s.MinReputation);
                                               case "discountValue":
                                                   return voucherFilterModel.OrderByDescending
                                                       ? s.OrderByDescending(s => s.DiscountValue)
