8eef1d6cc3d4a5009e65ef61dc3cbde6c6ec38f4
tinhcao <trancao134@gmail.com>
2025-04-23 23:25:15 +0700

    feat: add service

---
+++ Chillde.Services/Models/PackageModels/PackageAddAllModel.cs
@@ -0,0 +1,16 @@
+﻿using Chillde.Services.Models.PackageFeatureModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chillde.Services.Models.PackageModels
+{
+    public class PackageAddAllModel : PackageAddModel
+    {
+        public Guid PackageId { get; set; }
+        public Guid ServiceId { get; set; }
+        public List<PackageFeatureAddModel> PackageFeatureModels { get; set; } = new List<PackageFeatureAddModel>();
+    }
+}
