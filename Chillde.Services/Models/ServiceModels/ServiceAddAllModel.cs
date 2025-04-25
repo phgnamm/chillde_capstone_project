0e7c138b0699f3890c3220ed178dbf963f3b74da
Tn Lc_ <lamtanloc3@gmail.com>
2025-04-25 20:29:11 +0700

    Add mappings for new package and feature models
    Updated `MapperProfile` to include mapping for `PackageAddWithIdModel`.
    Replaced lists in `ServiceAddAllModel` with new models and added their definitions.
    Refactored `ServiceService` to streamline package and feature handling, removing commented-out code and ensuring correct property mapping.

--- Chillde.Services/Models/ServiceModels/ServiceAddAllModel.cs
+++ Chillde.Services/Models/ServiceModels/ServiceAddAllModel.cs
@@ -1,7 +1,10 @@
-﻿using Chillde.Services.Models.FeatureModels;
+﻿using Chillde.Repositories.Enums;
+using Chillde.Services.Models.FeatureModels;
+using Chillde.Services.Models.PackageFeatureModels;
 using Chillde.Services.Models.PackageModels;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +13,50 @@ namespace Chillde.Services.Models.ServiceModels
 {
     public class ServiceAddAllModel : ServiceAddModel
     {
-        public List<PackageAddModel> PackageAddAllModels { get; set; } = new List<PackageAddModel>();
-        public List<FeatureAddModel> FeatureAddModels { get; set; } = new List<FeatureAddModel>();
+        public List<PackageAddWithIdModel> PackageAddAllModels { get; set; } = new List<PackageAddWithIdModel>();
+        public List<FeatureAddWithIdModel> FeatureAddModels { get; set; } = new List<FeatureAddWithIdModel>();
+    }
+
+    public class PackageAddWithIdModel
+    {
+        public Guid PackageId { get; set; }
+        public PackageName Name { get; set; }
+        [Required(ErrorMessage = "Package's description is required.")]
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int? DeliveryTime { get; set; }
+        public int? SketchRevision { get; set; }
+        public TimeSpan ResponseTime { get; set; }
+        public int MinQuantity { get; set; }
+        public int? MaxQuantity { get; set; }
+        [Required]
+        public required List<PackageFeatureAddWithPackageIdModel> PackageFeatureAddModels { get; set; }
+    }
+
+    public class FeatureAddWithIdModel
+    {
+        [Required]
+        public Guid FeatureId { get; set; }
+        [Required]
+        public required string Name { get; set; }
+        public string? Question { get; set; }
+        public MediaType QuestionType { get; set; }
+        public bool IsInformationRequired { get; set; }
+        public bool IsQuantity { get; set; }
+        public int Index { get; set; }
+    }
+
+    public class PackageFeatureAddWithPackageIdModel
+    {
+        public Guid? FeatureId { get; set; }
+        // [Required]
+        public string? Name { get; set; }
+        public bool IsExtra { get; set; }
+        public decimal? AdditionalCost { get; set; }
+        public int? AdditionalDay { get; set; }
+        public bool? IsChecked { get; set; }
+        public int MinQuantity { get; set; }
+        public int? MaxQuantity { get; set; }
+        public int Index { get; set; }
     }
 }
