c9b54bea041a6621253c16d44ceb283ec6b8e871
tinhcao <trancao134@gmail.com>
2025-05-03 09:18:59 +0700

    fix: change threshold

--- Chillde.Services/Services/ServiceService.cs
+++ Chillde.Services/Services/ServiceService.cs
@@ -1636,7 +1636,7 @@ namespace Chillde.Services.Services
                         pageSize: 10000
                      );
 
-                    var threshold = 0.8;
+                    var threshold = 0.5;
                     var results = services.Data
                         .Where(s => CosineSimilarity(averageEmbedding, s.EmbeddingVector) >= threshold)
                         .Select(s => new ServiceModel
